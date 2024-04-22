1fedc7231b53697b0de94731b362bcbbae813700
JULIA SILVA LIMA <021.898697@senaicimatec.edu.br>
2024-04-22 14:22:15 -0300

    camile 2 alteracao

---
+++ src/components/logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logotur from '.../src/components/logotur.png';
+
+function logotur() {
+  return (
+    <div>
+      <h2>a</h2>
+      <img src={logotur} alt="logo" />
+    </div>
+  );
+}
+
+export default logotur;
